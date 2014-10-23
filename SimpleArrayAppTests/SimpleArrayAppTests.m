dd7993053f826812cb30e7132b6278e7f442d146
Jeffrey Linwood <jlinwood@gmail.com>
2014-10-23 15:20:35 -0500

    Initial Commit

---
+++ SimpleArrayAppTests/SimpleArrayAppTests.m
@@ -0,0 +1,34 @@
+//
+//  SimpleArrayAppTests.m
+//  SimpleArrayAppTests
+//
+//  Created by Jeffrey Linwood on 10/23/14.
+//  Copyright (c) 2014 Jeff Linwood. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface SimpleArrayAppTests : XCTestCase
+
+@end
+
+@implementation SimpleArrayAppTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
